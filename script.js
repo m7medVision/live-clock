6015a43e2644cb2222e24eb0b11863071c1bde24
Mohammed~ <88824957+majhcc@users.noreply.github.com>
2021-09-25 15:31:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var clock = new Vue({
+    el: '#clock',
+    data: {
+        time: '',
+        date: ''
+    }
+});
+
+var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+var timerID = setInterval(updateTime, 1000);
+updateTime();
+function updateTime() {
+    var cd = new Date();
+    var hourso = cd.getHours()-12
+    if (hourso < 0){
+        var time1 = cd.getHours()
+        var m = " AM"
+    }
+    else {
+        var time1 = hourso
+        var m = " PM"
+    }
+    clock.time = zeroPadding(time1, 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2) + zeroPadding(m) ;
+    clock.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+};
+
+function zeroPadding(num, digit) {
+    var zero = '';
+    for(var i = 0; i < digit; i++) {
+        zero += '0';
+    }
+    return (zero + num).slice(-digit);
+}
